f8919c47d927d83bff7b98e4726c6dc4e3ee9536
Tim McCarter <tmccarter@lampsplus.com>
2018-09-19 16:13:51 -0700

    adding action links on page and wrapping stuff in functions

--- web-usb/scripts/app.js
+++ web-usb/scripts/app.js
@@ -1,9 +1,21 @@
 (() => {
-	const devices = navigator.usb.getDevices().then(devices => {
-		console.log(devices);
-		devices.map(device => {
-			console.log(device.vendorId, device.productId, device.productName);
+	const requestDevicesLink = document.getElementById('requestDevices');
+	requestDevicesLink.onclick = requestDevices;
+	const getDevicesLink = document.getElementById('getDevices');
+	getDevicesLink.onclick = getDevices;
+	function requestDevices(e) {
+		e.preventDefault();
+		const device = navigator.usb.requestDevice({ filters: [] }).then(device => {
+			console.log(device);
 		});
-	});
-	console.log(devices);
+	}
+	function getDevices(e) {
+		e.preventDefault();
+		const devices = navigator.usb.getDevices().then(devices => {
+			console.log(devices);
+			devices.map(device => {
+				console.log(device.vendorId, device.productId, device.productName);
+			});
+		});
+	}
 })();
