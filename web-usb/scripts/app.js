025f5c2ffed4c3b2727062ff6857d212b1f82125
Tim McCarter <tmccarter@lampsplus.com>
2018-09-19 17:10:48 -0700

    improved logging, added some whitespace to the js file

--- web-usb/scripts/app.js
+++ web-usb/scripts/app.js
@@ -1,8 +1,10 @@
 (() => {
 	const requestDevicesLink = document.getElementById('requestDevices');
 	requestDevicesLink.onclick = requestDevices;
+
 	const getDevicesLink = document.getElementById('getDevices');
 	getDevicesLink.onclick = getDevices;
+
 	function requestDevices(e) {
 		e.preventDefault();
 		const device = navigator.usb
@@ -14,13 +16,23 @@
 				console.error(`ERROR: ${err}`);
 			});
 	}
+
 	function getDevices(e) {
 		e.preventDefault();
-		const devices = navigator.usb.getDevices().then(devices => {
-			console.log(devices);
-			devices.map(device => {
-				console.log(device.vendorId, device.productId, device.productName);
+		const devices = navigator.usb
+			.getDevices()
+			.then(devices => {
+				console.log(devices);
+				devices.map(device => {
+					console.log(`VendorId: ${device.vendorId}`);
+					console.log(`ProuctId: ${device.productId}`);
+					console.log(`ManufacturerName: ${device.manufacturerName}`);
+					console.log(`ProductName: ${device.productName}`);
+					console.log('------------------------------');
+				});
+			})
+			.catch(err => {
+				console.error(`ERROR: ${err}`);
 			});
-		});
 	}
 })();
